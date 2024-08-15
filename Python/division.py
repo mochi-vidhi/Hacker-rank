90c6f557c2c0562a5f400f438c98a415dad29665
Mochi Vidhi <148563893+mochi-vidhi@users.noreply.github.com>
2024-08-15 17:51:01 +0000

    Questions

---
+++ Python/division.py
@@ -0,0 +1,11 @@
+# Example
+
+# The result of the integer division a//b .
+# The result of the float division is  a/b.
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
